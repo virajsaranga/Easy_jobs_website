d8265592914d24c16f258f5b915e7013a2164be6
virajsaranga <virajridi@gmail.com>
2025-04-28 10:30:21 +0530

    app.js updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './component/Header';
+import HeroSection from './component/HeroSection';
 
 function App() {
  
 
   return (
-   
+   <>
     <Header />
+    <HeroSection />
+    </>
   
   )
 }
