8d7e597f429d1668ccc155ce2af86062abc13cdd
virajsaranga <virajridi@gmail.com>
2025-06-13 10:54:11 +0530

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-// Header component for the Jobify application
 const Header = () => {
   return (
     <AppBar
