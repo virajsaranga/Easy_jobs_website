b684537280b9f36faddde42130f9ead4d00e2015
virajsaranga <virajridi@gmail.com>
2025-06-16 16:09:05 +0530

    developed manage jobs page

---
+++ src/admin/pages/ManageJobs.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import JobPostForm from '../components/JobPostForm';
+import JobListAdmin from '../components/JobListAdmin';
+
+const ManageJobs = () => {
+  return (
+    <Box sx={{ p: 4 }}>
+      <Typography variant="h4" gutterBottom>Manage Jobs</Typography>
+      <JobPostForm />
+      <JobListAdmin />
+    </Box>
+  );
+};
+
+export default ManageJobs;
